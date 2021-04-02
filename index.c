baf131adcb4cdb6f6d6cbfefdd50b0db97eb9b79
David Sotto Mayor <ddsmmf@gmail.com>
2021-04-02 11:12:04 -0400

    Language C - Arithmetic operators.

--- index.c
+++ index.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /*
-Program Name: Language C - the type of variables and best practices.
-Goal: Understand how to declare variables and use the best practices in C.
-References: https://www.youtube.com/watch?v=5f_NSd3nnGo&list=PLgMem-KiO8qFk4S62AjdYdzSkAIsxVmFq&index=7&t=3784s, 
-            http://linguagemc.com.br/boas-praticas-de-programacao-em-linguagem-c/, 
-            http://excript.com/linguagem-c/nomenclatura-variavel-c.html
+Program Name: Language C - Arithmetic operators.
+Goal: Understand the arithmetc operators.
+References: https://www.youtube.com/watch?v=JDkHRHxOsGU&list=PLgMem-KiO8qFk4S62AjdYdzSkAIsxVmFq&index=8.
 
 David Sotto Mayor
 */
@@ -33,33 +31,23 @@ David Sotto Mayor
 
 int main() {
 
-    // To name variables I will adopt camelCase;
-
-    /*
-        Special characters: - ç - ~ - ´ - ' - " - % - & - ( - ) - { - } - [ - ] :
-        Numeric characters: - + - - - * - /
-        We cannot use these characters to define variables.
-
-        The use of numbers is allowed in the composition of names, as long as this is not the first character.
-        nameOfVariable100 (allowed) and 100NameOfVariable (not allowed)
-
-        We can define variable names using an underscore, both at the beginning and at the end of the name.
-        _name (allowed) and name_ (allowed)
+    /* Arithmetic operators
+    - Sum: +
+    - Subtraction: -
+    - Multiplication: *
+    - Division: /
+    - module: %
     */
 
-   // How to declare multiple variables with the same characteristics
-   int number, counter, accumulator;
+    float average, note1, note2, note3;
+    float AMOUNT_NOTES = 3.0;
+    
+    note1 = 7.5;
+    note2 = 6.7;
+    note3 = 8.3;
 
-   // The main types of variables in C
+    average = (note1 + note2 + note3) / AMOUNT_NOTES;
 
-    float GRAVITY = 9.8; // constant
-    int x; // integer (4 bytes)
-    unsigned int y; // unsigned integer (4 bytes)
-    unsigned long int z; // unsigned log integer (8 bytes)
-    long int a; // long integer (8 bytes)
-    short int b; // short integer (2 bytes)
-    char c; // (1 bytes)
-    unsigned char d; // (1 bytes)
-    float e; // comma numbers (4 bytes)
-    double f; // comma numbers (8 bytes)
+    printf("Notes: %.2f, %.2f, %.2f and Average: %.2f\n", average, note1, note2, note3);
+
 }
