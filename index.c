9d6edaefa98632787c7f817f49e8f738a34aba8b
David Sotto Mayor <ddsmmf@gmail.com>
2021-04-16 09:30:52 -0400

    Language C - String manipulation using native functions.

--- index.c
+++ index.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+// To work with strings, we need add a new include
+#include <string.h>
+
 /*
-Program Name: Language C - Knowing strings.
-Goal: Initializing with strings, inputting and displaying strings.
-References: https://www.youtube.com/watch?v=fsfn-jIidNw&list=PLgMem-KiO8qFk4S62AjdYdzSkAIsxVmFq&index=23.
+Program Name: Language C - String manipulation using native functions.
+Goal: Understanding the handling of strings: strcmp, strcpy, strlen and others.
+References: https://www.youtube.com/watch?v=E1sLEGWb9y8&list=PLgMem-KiO8qFk4S62AjdYdzSkAIsxVmFq&index=24.
 
 David Sotto Mayor
 */
@@ -34,6 +37,7 @@ int main() {
     // Declaring the arrays of string as CHAR.
     char name[100];
     char major[100];
+    unsigned stringLength;
 
     // Inputting data in them, to read strings we use the combination %[^\n]
 
@@ -43,9 +47,42 @@ int main() {
     printf("What is your major? ");
     scanf("%[^\n]%*c", major);
 
-    //Now, we will display the string. For this, utilize %s
+    printf("Hi! I'm %s and my major is %s. \n\n", name, major);
 
-    printf("Hi! I'm %s and my major is %s.", name, major);
+
+    //We use strlen(param) to get the length of string
+
+    stringLength = strlen(name);
+    printf("The length of the name is %u \n", stringLength);
+
+    printf("The length of the major is %lu \n", strlen(major));
+
+
+    // When we want to compare strings, we use strcmp(param1, param2). This function returns -1, 0, 1 regarding the param1 and param2
+    // Look the exemple
+
+    if(strcmp(name, major) == 0){
+        printf("The %s and $s are the same string!\n\n", name, major);
+    }else if( strcmp(name, major) == -1 ){
+         printf("The %s comes before the %s.\n\n", name, major);
+    }else{
+         printf("The %s comes after the %s.\n\n", name, major);
+    }
+    // There is also a variation of this function, and is strncmp(param1, param2, n). Here, the functions compares only the Nth
+    // position of the strings!
+
+
+    // To copy a string, we use the strcpy(param1, param2)
+    strcpy(name, major);
+    // There is also a variation of this function, and is strncpy(param1, param2, n). Here, the functions copies only the Nth
+    // position of the string!
+
+
+    // To concatenate, we use strcat(param1, param2)
+    strcat(name, major);
+
+
+
 
 }
     
